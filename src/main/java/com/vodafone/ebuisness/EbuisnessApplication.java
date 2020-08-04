3bf248301a0ad9249e5f7bf798a7b7a2d57239ba
navras <moamen.zakaria@gmail.com>
2020-08-04 22:16:44 +0200

    init Skeleton

---
+++ src/main/java/com/vodafone/ebuisness/EbuisnessApplication.java
@@ -0,0 +1,13 @@
+package com.vodafone.ebuisness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbuisnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbuisnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vodafone/ebuisness/EbuisnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vodafone.ebuisness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbuisnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
